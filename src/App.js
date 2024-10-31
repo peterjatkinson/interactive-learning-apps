3d9ad0061c4b708896550e280dcbaa7658270c3d
Peter Atkinson <peterjatkinson@gmail.com>
2024-10-31 12:01:45 +0000

    Homepage with links to apps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import PositioningStatementCreator from './apps/PositioningStatementCreator';
 import AdvertisingTimeline from './apps/AdvertisingTimeline';
 
@@ -7,7 +7,29 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Welcome to Interactive Learning Apps</h1>} />
+        <Route
+          path="/"
+          element={
+            <div style={{ textAlign: 'center', padding: '2rem' }}>
+              <h1>Welcome to Interactive Learning Apps</h1>
+              <p>Select an app to get started:</p>
+              <nav style={{ marginTop: '1rem' }}>
+                <ul style={{ listStyle: 'none', padding: 0 }}>
+                  <li style={{ marginBottom: '1rem' }}>
+                    <Link to="/positioning-statement" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+                      Positioning Statement Creator
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/advertising-timeline" style={{ fontSize: '1.2rem', textDecoration: 'underline' }}>
+                      Advertising Timeline
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          }
+        />
         <Route path="/positioning-statement" element={<PositioningStatementCreator />} />
         <Route path="/advertising-timeline" element={<AdvertisingTimeline />} />
       </Routes>
