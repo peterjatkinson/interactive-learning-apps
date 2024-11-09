35c4b09d405b30e2092b05173a193c7e50d38e05
Peter Atkinson <peterjatkinson@gmail.com>
2024-11-09 13:41:54 +0000

    Iframe resizing test 2

--- src/apps/PositioningStatementGlobal.js
+++ src/apps/PositioningStatementGlobal.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { requestResize } from '../resizeHelper';
 
 const PositioningStatementGlobal = () => {
